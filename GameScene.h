0cc2d7936069d21ac8706dc1494409eaa58cb866
ヒガシ_サチエ <k024g0072@g.neec.ac.jp>
2026-05-07 04:40:17 +0900

    シーン遷移

--- GameScene.h
+++ GameScene.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Boss.h" 
+#include "BaseScene.h"
 
 // 既存のエンジンのクラス
 class Player;
@@ -7,21 +8,22 @@ class ModelCommon;
 class Object3dCommon;
 class Camera;
 
-class GameScene {
+class GameScene :public BaseScene {
 public:
-    void Initialize(Object3dCommon* object3dCommon, Camera* camera);
-    void Update(Player* player);
-    void Draw();
-  
-    ~GameScene();
+	void Initialize(Object3dCommon* object3dCommon, Camera* camera)override;
+	void Update(Player* player)override;
+	void Draw()override;
+	void Finalize() override;
+
+	~GameScene();
 
 private:
-    // ボスのインスタンス
-    Boss* boss_ = nullptr;
+	// ボスのインスタンス
+	Boss* boss_ = nullptr;
 
-    Object3d* field_ = nullptr;
+	Object3d* field_ = nullptr;
 
-   
-    ModelCommon* modelCommon_ = nullptr;
-    Object3dCommon* object3dCommon_ = nullptr;
+
+	ModelCommon* modelCommon_ = nullptr;
+	Object3dCommon* object3dCommon_ = nullptr;
 };
